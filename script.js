73dfa30ea0523a442773eae978e68dbc943888d3
Thiago Mota <thiago.you23@gmail.com>
2025-05-08 00:46:53 -0300

    Adicionamos a interação do header

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-const divParaVoce = document.getElementById("div-para-voce");
+function toggleVisibility(
+  divPrincipal,
+  setaPrincipal,
+  divSecundaria,
+  setaSecundaria,
+  divTercearia,
+  setaTercearia
+) {
+  const divPrincipalId = document.getElementById(divPrincipal);
+  const divSecundariaId = document.getElementById(divSecundaria);
+  const divTerceariaId = document.getElementById(divTercearia);
+  const setaPrincipalId = document.getElementById(setaPrincipal);
+  const setaSecundariaId = document.getElementById(setaSecundaria);
+  const setaTerceariaId = document.getElementById(setaTercearia);
 
-function ShowDiv(divParaVoce) {
-  if (divParaVoce.style.display == "hidden") {
-    divParaVoce.style.display = "block";
-  } else if (divParaVoce.style.display == "block") {
-    divParaVoce.style.display = "hidden";
+  if (divPrincipalId.classList.contains("hidden")) {
+    divPrincipalId.classList.remove("hidden");
+    setaPrincipalId.classList.add("rotate-180");
+    divPrincipalId.classList.add("flex");
+
+    divSecundariaId.classList.remove("flex");
+    setaSecundariaId.classList.remove("rotate-180");
+    divSecundariaId.classList.add("hidden");
+
+    divTerceariaId.classList.remove("flex");
+    setaTerceariaId.classList.remove("rotate-180");
+    divTerceariaId.classList.add("hidden");
+  } else {
+    divPrincipalId.classList.remove("flex");
+    setaPrincipalId.classList.remove("rotate-180");
+    divPrincipalId.classList.add("hidden");
   }
 }
